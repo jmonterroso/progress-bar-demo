d279a7846e5619734d9f8279e65dc2c38adca0d0
Jayson Monterroso <jayson.monterroso@gmail.com>
2017-07-13 11:32:52 -0600

    initial commit

--- source/js/views/Dashboard/index.jsx
+++ source/js/views/Dashboard/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { testAction, testAsync } from 'actions/app';
 import Progress from 'components/Progress/Progress';
 
@@ -17,18 +17,6 @@ export default class Dashboard extends Component {
 
     this.handleAsyncButtonClick = this.handleAsyncButtonClick.bind(this);
     this.handleTestButtonClick = this.handleTestButtonClick.bind(this);
-  }
-
-  handleAsyncButtonClick() {
-    const { dispatch } = this.props;
-
-    dispatch(testAsync());
-  }
-
-  handleTestButtonClick() {
-    const { dispatch } = this.props;
-
-    dispatch(testAction());
   }
 
   render() {
