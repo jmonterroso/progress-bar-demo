94a6e37e44f45f70450eaea7472584c6ff0cd227
Jayson Monterroso <jayson.monterroso@gmail.com>
2017-07-13 11:35:38 -0600

    initial commit

--- source/js/views/Dashboard/index.jsx
+++ source/js/views/Dashboard/index.jsx
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import Progress from 'components/Progress/Progress';
-
 export default class Dashboard extends Component {
   constructor() {
-
     super();
-
     this.handleAsyncButtonClick = this.handleAsyncButtonClick.bind(this);
     this.handleTestButtonClick = this.handleTestButtonClick.bind(this);
   }
-
   render() {
     return (
       <div className='container'>
